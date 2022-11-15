199a233291cc4437235fdb080eeffde8a76de8ed
Justus Becker-Rabbenstein <justusbera@googlemail.com>
2022-11-15 12:05:39 +0100

    api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,39 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const products = ["Handy", "Banane"];
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.method);
+  console.log(req.body);
+  console.log(req.headers);
+  console.log(req.query);
+
+  /* Lange Schreibweise
+  if (req.method === 'GET') {
+    res.status(200).json(products);
+  } else if (req.method === 'POST') {
+    res.status(200).json({message: "Product successfully saved"});
+  } else {
+    res.status(400).json({error: "method not supported"});
+  }
+  */
+
+  /* kurze Schreibweise */
+  switch (req.method) {
+    case "PUT":
+    case "PATCH":
+      res.status(200).json({ message: "PUT or PATCH called." });
+      break;
+    case "GET":
+      res.status(200).json(products);
+      break;
+    case "POST":
+      res.status(200).json({ message: "Product successfully saved" });
+      break;
+    default:
+      res.status(400).json({ error: "method not supported" });
+  }
+
+  /* oder case ... return res.status(200).json(products); statt break */
+
+  res.status(200).json(products);
 }
